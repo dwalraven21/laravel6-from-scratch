d90668b34ff363292ed72bff7d7f5d78e5a74863
Danielle Walraven <dwalraven21@gmail.com>
2020-01-12 14:04:31 -0500

    Pass request data to views.

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $name = request('name');
 
-Route::get('/welcome', function () {
-    return view('welcome');
+    return view('test', [
+        'name' => $name
+    ]);
 });
 
