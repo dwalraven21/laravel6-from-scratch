b71274f372b7f28f5714d7a9255cf8199b9a62a7
Danielle Walraven <dwalraven21@gmail.com>
2020-01-12 15:33:27 -0500

    Made query more 'Eloquent'.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,22 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use App\Post;
+
 class PostsController extends Controller
 
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-
-        //dd => "dump and die" inspects the var and kills execution.
-        // dd($post);
-
-        if (! $post) {
-            abort(404);
-        }
-
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
